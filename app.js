e0ff0b2258dd87d9499ab8205bcc8796b3aaa0b7
antoni-devlin <antoni.devlin@gmail.com>
2023-05-24 15:41:08 +0100

    Removed example express routes, and added a /doughnuts/random route that serves up a random doughnut

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ import express from "express";
 import { doughnuts } from "./doughnutData.js";
 const app = express();
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 app.get("/", (req, res) => {
+  return res.send("Server running!");
+});
+
+app.get("/doughnuts/list", (req, res) => {
   return res.send(doughnuts);
 });
 
-// app.post("/", (req, res) => {
-//   return res.send("Received a POST HTTP method");
-// });
-
-// app.put("/", (req, res) => {
-//   return res.send("Received a PUT HTTP method");
-// });
-
-// app.delete("/", (req, res) => {
-//   return res.send("Received a DELETE HTTP method");
-// });
+app.get("/doughnuts/random", (req, res) => {
+  const data = doughnuts[getRandomInt(Object.keys(doughnuts).length)];
+  return res.send(data);
+});
 
 app.listen(3000, () => console.log(`Example app listening on port port!`));
